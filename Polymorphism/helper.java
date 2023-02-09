6f39903556540bb765cd6071917f90bea9343d5e
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-02-09 12:16:22 +0530

    Polymorphism

---
+++ Polymorphism/helper.java
@@ -0,0 +1,12 @@
+package Polymorphism;
+
+public class helper {
+    
+    static int multiply(int a, int b){
+        return a*b;
+    }
+
+    static double multiply(double a, double b){
+        return a*b;
+    }
+}
---
+++ Polymorphism/testClass.java
@@ -0,0 +1,10 @@
+package Polymorphism;
+
+public class testClass {
+
+    public static void main(String[] args) {
+        System.out.println(helper.multiply(2, 4));
+        System.out.println(helper.multiply(2.0, 4.0));
+    }
+    
+}
